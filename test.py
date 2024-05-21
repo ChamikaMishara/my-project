9787bc1f293e61536b11821c13da6caf9b79319b
ChamikaMishara <pasinduchamikamishara2001@gmail.com>
2024-05-21 23:05:50 +0530

    Creating Main Class

---
+++ test.py
@@ -0,0 +1,17 @@
+# main.py
+
+from vehicles import list_vehicles
+from fruits import list_fruits
+from animals import list_animals
+
+def main():
+    print("Welcome to the main branch")
+    print("\nVehicles:")
+    list_vehicles()
+    print("\nFruits:")
+    list_fruits()
+    print("\nAnimals:")
+    list_animals()
+
+if __name__ == "__main__":
+    main()
