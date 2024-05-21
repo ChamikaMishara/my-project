3097c3b11d9b8f5985b5055927f1936260dda152
Duvindu Sahan <100335279+duvindusahan@users.noreply.github.com>
2024-05-21 23:37:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from vehicles import list_vehicles
 from fruits import list_fruits
-from animals import list_animals
+from animals import list_animals, list_birds
 
 def main():
     print("Welcome to the main branch")
@@ -10,6 +10,8 @@ def main():
     list_fruits()
     print("\nAnimals:")
     list_animals()
+    print("\nBirds:")
+    list_birds()
 
 if __name__ == "__main__":
     main()
